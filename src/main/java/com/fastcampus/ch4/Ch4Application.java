e187f05a0beae61b46cd1231cbc04f7480b290e2
KIM-BEOMSOO <clever0305@naver.com>
2024-05-20 14:32:16 +0900

    톰캣 동작 X로 변경 / EntityMangerFactory 주입

--- src/main/java/com/fastcampus/ch4/Ch4Application.java
+++ src/main/java/com/fastcampus/ch4/Ch4Application.java
@@ -1,13 +1,26 @@
 package com.fastcampus.ch4;
 
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ch4Application {
+public class Ch4Application implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ch4Application.class, args);
+	@Autowired
+	EntityManagerFactory emf;
+
+	public static void main(String[] args){
+		SpringApplication app = new SpringApplication(Ch4Application.class);
+		app.setWebApplicationType(WebApplicationType.NONE);
+		app.run(args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("emf = " + emf);
+	}
 }
